fa044fab5f4e010dcaff3c1b9eec3edc294cd6cd
Aloxion <ansan21@student.sdu.dk>
2024-11-25 12:10:17 +0100

    Removed unneccessary method and config values

--- kafka/producer.py
+++ kafka/producer.py
@@ -3,14 +3,8 @@ class KafkaProducer:
     def __init__(self):
         self.producer_config = {
             'bootstrap.servers': 'kafka-service:9092',
-            'group.id': 'producer-group',
-            'auto.offset.reset': 'earliest',
         }
         self.producer = Producer(self.producer_config)
-
-    def subscribe(self, topic: str):
-        self.producer.subscribe([topic])
-        print(f"Subscribed to topic: {topic}")
 
     def produce_message(self, topic: str, message: str):
         self.producer.produce(topic, message)
