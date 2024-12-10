466cdee3e2c48ca5166a593b35be4f76425bcc08
Haim-hub <niklas@haim.dk>
2024-12-10 08:03:28 +0100

    new predictor service

--- prediction/hdfs_connection.py
+++ prediction/hdfs_connection.py
@@ -24,7 +24,7 @@ def list_files(path):
 hdfs_path = "/user/root"  # Replace with the fixed HDFS path
 
 
-def load_country_data(country):
+def load_country_model(country):
     """Load and process data for a specific country from a PKL file in HDFS."""
     file_path = f"{hdfs_path}/models/{country}_model.pkl"
     try:
@@ -35,6 +35,40 @@ def load_country_data(country):
         return country_data
     except Exception as e:
         print(f"Error loading data for {country} from {file_path}: {e}")
+        return None
+
+def load_hdr_data():
+    """Load and process HDR data from a static path in HDFS."""
+    file_path = f"{hdfs_path}/hdr.json"
+    try:
+        with client.read(file_path) as file:
+            hdr_json_data = [json.loads(line) for line in file]
+        hdr_flattened = [
+            {
+                'country': country_data['country'],
+                'year': record['year'],
+                'value': record['value'],
+                'NOC': country_data['NOC']
+            }
+            for country_data in hdr_json_data for record in country_data['data']
+        ]
+        return pd.DataFrame(hdr_flattened)
+    except Exception as e:
+        print(f"Error loading HDR data from {file_path}: {e}")
+        return None
+    
+def get_hdr_value(country, year):
+    """Get the HDR value for a specific country and year."""
+    hdr_data = load_hdr_data()
+    if hdr_data is not None:
+        result = hdr_data[(hdr_data['country'] == country) & (hdr_data['year'] == year)]
+        if not result.empty:
+            return result.iloc[0]['value']
+        else:
+            print(f"No HDR data found for country: {country} and year: {year}")
+            return None
+    else:
+        print("HDR data could not be loaded.")
         return None
 
 
--- prediction/predictor.py
+++ prediction/predictor.py
@@ -3,23 +3,26 @@ import pandas as pd
 from hdfs import InsecureClient
 import joblib
 
-from hdfs_connection import load_country_data
+from hdfs_connection import load_country_model, get_hdr_value
 
 def predict(country, year):
     """Predict the number of medals for a specific country in a given year."""
     # Load the model for the specified country
-    country_data = load_country_data(country)
-    print(country_data)
-    if country_data is None:
+    country_model = load_country_model(country)
+    if country_model is None:
         return "No Country data found"
-    
+    hdr_value = get_hdr_value(country, year)
     # Prepare the input data for prediction
-    X_pred = pd.DataFrame({'Year': [year], 'value': [country_data['value'].iloc[-1]]})
+    X_pred = pd.DataFrame({'Year': [year], 'value': [hdr_value]})
+
+    #You need to load in the hdr data
+    #Then find the value for the year
+    # and then create a dataframe that looks like the training data
+    # Then make prediction
     
     # Make the prediction
-    model = country_data['model'].iloc[-1]
-    y_pred = model.predict(X_pred)
+    y_pred = country_model.predict(X_pred)
     
-    return y_pred[0]
+    return y_pred
 
 print(predict('USA', 2020))
