8ea7aa881a3d16255cc0e1fbc01420fe5e620378
Haim-hub <niklas@haim.dk>
2024-12-10 08:23:44 +0100

    set up fastAPI

--- prediction/main.py
+++ prediction/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 import uvicorn
+from predictor import predict
 
 app = FastAPI()
 
-def predict(country: str, year: int) -> str:
-    # Dummy prediction logic
-    return f"Prediction for {country} in {year}"
 
 @app.get("/predict")
 def get_prediction(country: str, year: int):
